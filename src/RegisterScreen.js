b6360617487ee3d3a6e87c6365e5f8cdd3a1c784
alexis.pelissier <alexis.pelissier@edu.itescia.fr>
2023-01-05 10:00:01 +0100

    test

--- src/RegisterScreen.js
+++ src/RegisterScreen.js
@@ -5,7 +5,8 @@
  * @format
  * @flow strict-local
  */
-import React from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {
   SafeAreaView,
   View,
@@ -19,38 +20,106 @@ import {
   ScrollView,
 } from 'react-native';
 
+// Register Successful
+const onPress = (firstName, lastName, password) =>
+  Alert.alert(
+    'Inscription effectuée',
+    `Bonjour ${firstName} ${lastName}, votre mot de passe est ${password}`,
+    {text: 'OK', onPress: () => console.log('OK Pressed')},
+  );
+/**
+ * Register Screen Component
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const RegisterScreen = () => {
-  const onPress = () =>
-    Alert.alert('Felicitation', 'Inscription enregistrée', [
-      {text: 'OK', onPress: () => console.log('OK Pressed')},
-    ]);
+  //Definition of states
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordIsValid, setPasswordIsValid] = useState(true);
+  let firstlaunch = true;
+
+  // password Validation
+  useEffect(() => {
+    if (!firstlaunch) {
+      if (password.length >= 3) {
+        setPasswordIsValid(true);
+      } else {
+        setPasswordIsValid(false);
+      }
+    } else {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      firstlaunch = false;
+    }
+  }, [password]);
+
+  // confirmpassword validation useMemo
+  const confirmPasswordIsValid = useMemo(() => {
+    return passwordIsValid && password === confirmPassword;
+  }, [confirmPassword, password, passwordIsValid]);
+
+  // Form validation callback
+  const validateForm = useCallback(() => {
+    if (
+      passwordIsValid &&
+      confirmPasswordIsValid &&
+      firstname.length > 1 &&
+      lastname.length > 1
+    ) {
+      onPress(firstname, lastname, password);
+    }
+  }, [passwordIsValid, confirmPasswordIsValid, firstname, lastname, password]);
 
   return (
     <SafeAreaView style={styles.screen}>
       <Text style={{fontWeight: 'bold', fontSize: 25, alignSelf: 'center'}}>
         Inscription
       </Text>
+
       <Image
         style={styles.profilePicture}
         source={{
           uri: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F5%2FProfile-Avatar-PNG.png&f=1&nofb=1&ipt=54410382c540772fe9f2500d35d85ff329758c284d9ae3ec37f33edd083c4233&ipo=images',
         }}
       />
+
       <ScrollView style={{flex: 1, alignSelf: 'center'}}>
-        <TextInput style={styles.inputText} placeholder="Prénom" />
-        <TextInput style={styles.inputText} placeholder="Nom" />
         <TextInput
           style={styles.inputText}
+          placeholder="Prénom"
+          value={firstname}
+          //onEndEditing={setFirstname}
+          onChangeText={setFirstname}
+        />
+        <TextInput
+          style={styles.inputText}
+          placeholder="Nom"
+          value={lastname}
+          onChangeText={setLastname}
+        />
+        <TextInput
+          style={passwordIsValid ? styles.inputText : styles.passwordIsNotValid}
           secureTextEntry={true}
           placeholder="Mot de passe"
+          value={password}
+          onChangeText={setPassword}
         />
         <TextInput
-          style={styles.inputText}
+          style={
+            confirmPasswordIsValid
+              ? styles.inputText
+              : styles.passwordIsNotValid
+          }
           secureTextEntry={true}
           placeholder="Confirmation du mot de passe"
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
         />
+
         <View style={{alignSelf: 'center', marginTop: 20}}>
-          <TouchableOpacity style={styles.sendStyle} onPress={onPress}>
+          <TouchableOpacity style={styles.sendStyle} onPress={validateForm}>
             <Text style={{color: 'darkgray'}}>Envoyer</Text>
           </TouchableOpacity>
         </View>
@@ -62,6 +131,17 @@ const RegisterScreen = () => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+  },
+  passwordIsValid: {},
+  passwordIsNotValid: {
+    backgroundColor: 'gainsboro',
+    paddingStart: 15,
+    height: 50,
+    width: 350,
+    borderRadius: 5,
+    borderWidth: 1,
+    marginTop: 35,
+    borderColor: 'red',
   },
   sendStyle: {
     borderColor: 'darkgray',
