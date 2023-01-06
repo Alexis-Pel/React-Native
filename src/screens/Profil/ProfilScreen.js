4b88f6e545049dd52682b2135bb47aba0e7299d8
angel_rafael <generalpompeyo@hotmail.com>
2023-01-06 13:27:20 +0100

    Navigation improvements

--- src/screens/Profil/ProfilScreen.js
+++ src/screens/Profil/ProfilScreen.js
@@ -1,50 +1,50 @@
-
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { deleteStorage, getData, storeData } from '../../helpers/StorageHelper';
+import React, {useCallback, useEffect, useState} from 'react';
+import {getData} from '../../helpers/StorageHelper';
+import {useDispatch} from 'react-redux';
+import {actions as loginActions} from '../../redux/reducers/logInReducer';
 import {
   SafeAreaView,
   View,
-  StyleSheet,
   Text,
   Image,
-  TextInput,
   TouchableOpacity,
-  KeyboardAvoidingView,
-  Alert,
   ScrollView,
 } from 'react-native';
- import styles from './ProfilStyles.js'
+import styles from './ProfilStyles.js';
 
-
-const ProfilScreen =  () => {
-
+const ProfilScreen = navigation => {
   const [pseudo, setPseudo] = useState();
   const [password, setPassword] = useState();
-  const [image, setImage] = useState('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F5%2FProfile-Avatar-PNG.png&f=1&nofb=1&ipt=54410382c540772fe9f2500d35d85ff329758c284d9ae3ec37f33edd083c4233&ipo=images');
+  const [image, setImage] = useState(
+    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F5%2FProfile-Avatar-PNG.png&f=1&nofb=1&ipt=54410382c540772fe9f2500d35d85ff329758c284d9ae3ec37f33edd083c4233&ipo=images',
+  );
   const [isHide, setIsHide] = useState(true);
 
   const getUser = async () => {
-    const user = await getData('user')
-    if (!user){
+    const user = await getData('user');
+    if (!user) {
       return;
     }
-    setPseudo(user.username)
-    setPassword(user.password)
-    setImage(user.image)
+    setPseudo(user.username);
+    setPassword(user.password);
+    setImage(user.image);
   };
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const hideAndShowPassword = useCallback(() => {
-    setIsHide(!isHide)
+    setIsHide(!isHide);
   });
 
   useEffect(() => {
-    getUser()
-  }, [])
+    getUser();
+  }, []);
 
   // Deconnect the user on remove
-  const removeSession = useCallback(() => {
-    deleteStorage('user');
-  });
+  const dispatch = useDispatch();
+  const logOutWithRedux = useCallback(() => {
+    dispatch(loginActions.logOut());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <SafeAreaView style={styles.screen}>
@@ -55,15 +55,16 @@ const ProfilScreen =  () => {
           source={{
             uri: image,
           }}
-          onPress={onPressImg}
         />
         <View style={styles.view}>
-          <Text style={styles.inputText}>{isHide ? password.replace(/./g, '*') : password}</Text>
+          <Text style={styles.inputText}>
+            {/* {isHide ? password.replace(/./g, '*') : password} */}
+          </Text>
           <TouchableOpacity onPress={hideAndShowPassword}>
             <Text style={styles.buttonHideShow}>👁️</Text>
           </TouchableOpacity>
         </View>
-        <TouchableOpacity style={styles.inputText} onPress={removeSession}>
+        <TouchableOpacity style={styles.inputText} onPress={logOutWithRedux}>
           <Text>Déconnecter</Text>
         </TouchableOpacity>
       </ScrollView>
--- src/screens/dashboard/Dashboard.js
+++ src/screens/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useCallback} from 'react';
 import React, {
   FlatList,
   Alert,
@@ -51,12 +51,18 @@ const Dashboard = navigation => {
     }
   };
 
+  const openDetailScreen = useCallback(() => {
+    navigation.navigation.navigate('ProfilScreen');
+  }, [navigation]);
+
   return (
     <SafeAreaView style={styles.mainSafeArea}>
       <View style={styles.titleView}>
         <Text style={styles.mainTitle}>Gamepedia</Text>
-        <TouchableOpacity style={styles.titleOpacity} onPress={dataFilter}>
-          <Text style={styles.profilText}>Profil</Text>
+        <TouchableOpacity
+          style={styles.titleOpacity}
+          onPress={openDetailScreen}>
+          <Text style={styles.profilText}>👁️</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.searchView}>
